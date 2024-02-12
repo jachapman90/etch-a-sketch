2d7ac3654c76508f26fb0becd77608ae33fbc6af
jachapman90 <jachapman.1990@gmail.com>
2024-02-12 15:23:42 -0500

    Add grid and mouseover event

--- script.js
+++ script.js
@@ -1,67 +1,23 @@
 
 const body = document.querySelector('body');
-const cont = document.querySelector('#container');
-const input = document.querySelector('#input');
-const start = document.querySelector('#start');
+const cont = document.querySelector('.container');
 
-let pixelCount;
-let row = document.createElement('div');
-
-start.addEventListener('click', () => {
-    drawRow();
-})
-
-
-function numCheck() {
-    pixelCount = input.value;
-    console.log(pixelCount);
-     if (isNaN(pixelCount)) {
-        wrong();
-     } else if (pixelCount > 100 || pixelCount < 0) {
-        wrong();
-     } else {
-        return(pixelCount);
-     };
-};
-
-function wrong() {
-    alert("Please enter a number between 1-100");
-};
-
-function drawPixel() {
-    let pixelCell = document.createElement('div');
-    pixelCell.id = 'pixelCell';
-    pixelCell.style.flex = '1 1 0px';
-    pixelCell.style.display = 'flex';
-    pixelCell.style.justifyContent = 'center';
-    pixelCell.style.alignItems = 'stretch';
-    pixelCell.style.boxSizing = 'border-box';
-    let pixel = document.createElement('div');
-    pixel.id = 'pixel';
-    pixel.style.display = 'flex';
-    pixel.style.justifyContent = 'center';
-    pixel.style.alignItems = 'center';
-    pixel.style.width = '100%';
-    pixel.style.paddingTop = '100%';
-    pixel.style.minWidth = '10px';
-    pixel.style.border = 'solid 2px';
-    pixel.style.borderColor = 'black';
-    pixel.style.boxSizing = 'border-box';
-    pixelCell.appendChild(pixel);
-    row.appendChild(pixelCell);
-};
-
-function drawRow() {
-    numCheck();
-    let i = pixelCount;
-    console.log(i);
-    row.style.display = 'flex';
-    row.style.justifyContent = 'space-evenly';
-    row.style.gap = '2px';
-    cont.appendChild(row);
-    while (i > 0) {
-        drawPixel();
-        console.log(i);
-        i--;
+function doIt() {
+    for (let i = 0; i < 16; i++) {
+        for (let i = 0; i < 16; i++) {
+        let div = document.createElement('div');
+        div.className = "pixel";
+        cont.appendChild(div);
+        };
     };
 };
+
+addEventListener("mouseover", (event) => {
+    let target = event.target;
+
+    switch(target.className) {
+        case 'pixel':
+        target.style.backgroundColor = "darkgrey";
+}});
+
+doIt();
